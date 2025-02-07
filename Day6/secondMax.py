1ed19f94b8c1358c21258d1899ba019a31d511b8
Abhishek19000 <abhiraj19000@gmail.com>
2025-02-07 23:37:19 +0530

    Day6> Second Max Element in array

--- Day6/secondMax.py
+++ Day6/secondMax.py
@@ -7,3 +7,26 @@ def secondMax(arr:list[int])->int:
     secondMax=arr[-2]
 
     return secondMax
+
+#Better approach: O(2n)
+
+def secondMax(arr:list[int])->int:
+    maxi=float('-inf')
+    secondMax=float('-inf')
+    n=len(arr)
+
+    for i in range(n):
+        if arr[i]>maxi:
+            maxi=max(maxi,arr[i])
+    
+    for i in range(n):
+        if arr[i]>secondMax and arr[i]!=maxi:
+            secondMax=arr[i]
+
+    return secondMax
+
+#Optimal approach: O(n)
+
+def secondMax(arr:list[int])->int:
+    maxi=float('-inf')
+    secondMax=float('-inf')
