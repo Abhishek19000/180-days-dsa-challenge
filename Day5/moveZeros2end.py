7a2c1507b87eded151a4c90c4b9b21cf6daf9675
Abhishek19000 <abhiraj19000@gmail.com>
2025-02-07 22:17:26 +0530

    Day5> Move zeros to the end of the array

--- Day5/moveZeros2end.py
+++ Day5/moveZeros2end.py
@@ -20,4 +20,23 @@ def moveZeros2end(arr:list[int])->list[int]:
 
     return arr
 
+#Optimal approach: O(n)
+
+def moveZeros2end(arr:list[int])->list[int]:
+
+    n=len(arr)
+    j=-1
+    for i in range(n):
+        if(arr[i]==0):
+            j=i
+            break
+
+    for i in range(j+1,n):
+        if arr[i]!=0:
+            arr[i],arr[j]=arr[j],arr[i]
+            j+=1
+    return arr
+
+
+
 
