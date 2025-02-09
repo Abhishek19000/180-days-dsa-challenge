565d132352d96128de4bda7f78f0e547da1cc9d3
Abhishek19000 <abhiraj19000@gmail.com>
2025-02-09 21:41:27 +0530

    Day7> Two Sum

---
+++ Day7/twoSum.py
@@ -0,0 +1,28 @@
+#Finding the two elements in the array whose sum is equal to k
+
+#BruteForce: O(n^2)
+
+def sumTwo(arr:list[int],k)->list[int]:
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if arr[i]+arr[j]==k:
+                return [arr[i],arr[j]]
+    
+    return [-1,-1]
+
+#Better approach : 
+
+def sumTwo(arr:list[int],k)->list[int]:
+
+    eleInMap={}
+
+    for i in range(len(arr)):
+        num=arr[i]
+        target=k-num
+
+        if target in eleInMap:
+            return [eleInMap[target],i]
+        
+        eleInMap[num]=i
+
+    return -1
