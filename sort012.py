97a1e8b03a2ca40f684f935f5afaf6719633977b
Abhishek19000 <abhiraj19000@gmail.com>
2025-02-10 19:39:20 +0530

    Day7> Sorting the array using the better approach

--- sort012.py
+++ sort012.py
@@ -6,3 +6,29 @@ def sort012(arr:list[int])->list[int]:
     arr.sort()
 
     return arr
+
+#Better approach: 
+
+def sort012(arr:list[int])->list[int]:
+    n = len(arr)
+    cnt=0
+    cnt1=0
+    cnt2=0
+    for i in range(n):
+        if arr[i]==0:
+            cnt+=1
+        elif arr[i]==1:
+            cnt1+=1
+        elif arr[i]==2:
+            cnt2+=1
+    
+    for j in range(cnt):
+        arr[j]=0
+
+    for j in range(cnt,cnt+cnt1):
+        arr[j]=1
+    
+    for j in range(cnt+cnt1,cnt+cnt1+cnt2):
+        arr[j]=2
+    
+    return arr
